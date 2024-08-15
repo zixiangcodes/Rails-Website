3611f51bd9b3c95061b900b8e91906db0591fe50
Zi Xiang <zixiang150193@hotmail.com>
2024-08-15 10:25:36 +0800

    Testing to see if pics can be uploaded to Render and persist.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :render
   config.active_storage.analyze_later = true
   config.active_storage.queues.analysis = :active_storage_analysis
   config.active_storage.queues.purge    = :active_storage_purge
