039a135b602c4c5b6a53ba207cc6efc9c019cd1f
Zi Xiang <zixiang150193@hotmail.com>
2024-08-22 14:14:33 +0800

    Update using API feature.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+    
+    # class ApplicationController < ActionController::Base
+    # Make ApplicationController inherit from ActionController::API instead of ActionController::Base. 
+    # As with middleware, this will leave out any Action Controller modules that provide 
+    # functionalities primarily used by browser applications.
+
 end
